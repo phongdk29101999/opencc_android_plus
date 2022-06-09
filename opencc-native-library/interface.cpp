f8a6d07070f5d16cf6aee38df2cc37fb7d24e86c
dolphin <adaikiss@163.com>
2022-06-09 17:29:31 +0800

    add build files

---
+++ opencc-native-library/interface.cpp
@@ -0,0 +1,44 @@
+#include <malloc.h>
+#include <cstring>
+#include "OpenCC/src/Converter.hpp"
+#include "OpenCC/src/Config.hpp"
+
+extern "C"
+{
+
+  const char* convert(const char* text, const char* configFile) {
+    opencc::Config config;
+    opencc::ConverterPtr converter = config.NewFromFile(configFile);
+
+    std::string s = converter->Convert(text);
+
+    char* r = strcpy(new char[s.length()], s.c_str());
+
+    return r;
+  }
+
+  char** convertList(char** list, int size, const char* configFile) {
+    opencc::Config config;
+    opencc::ConverterPtr converter = config.NewFromFile(configFile);
+    char** result = (char**)malloc(size * sizeof(char*));
+    for(int i = 0;i < size;i++) {
+      std::string s = converter->Convert((char*)list[i]);
+      result[i] = (char*) malloc(s.length());
+      strcpy(result[i], s.c_str());
+//      result[i][s.length()] = '\0';
+    }
+    return result;
+  }
+
+  void free_string(char *str) {
+      // Free native memory in C which was allocated in C.
+      free(str);
+  }
+
+  void free_string_array(char **array, int size) {
+      for(int i = 0;i < size;i++) {
+          free(array[i]);
+      }
+      free(array);
+  }
+}
